5a30c06ac6b37c1193f557d49cb0d80466e79e08
raghunathchanda <chandaraghunathreddy@gmail.com>
2023-01-25 07:46:42 +0530

    newaddedmsg

--- filename.py
+++ filename.py
@@ -1,9 +1,3 @@
-class A:
-    def m1(self,*x):
-        print("values is x:",x)
-    def m1(self,y,c):
-        print("values is y:", y)
-a = A()
-# a.m1()
-# a.m1(10)
-a.m1(10,20)
+x = 6
+y = 20
+print(x +y)
