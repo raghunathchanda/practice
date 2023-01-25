0e76fc7c35dcb5c0faa2bf197a27163902fe0e5b
raghunathchanda <123394812+raghunathchanda@users.noreply.github.com>
2023-01-25 06:12:49 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,8 +1,9 @@
+# biggest of two numbers code 
 x = input("enter a number :")
 y = input("enter another number :")
 if x > y :
     print(f"{x} is bigger than {y}")
 else:
     print(f"{y} is bigger than {x}")
-
+# biggest of three  numbers code 
 
